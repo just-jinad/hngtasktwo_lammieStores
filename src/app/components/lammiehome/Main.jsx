d05f4826b066f69bd257144329f8d72978c5dfbf
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-07-07 23:56:26 +0100

    corrected the navbar for homepage

--- src/app/components/lammiehome/Main.jsx
+++ src/app/components/lammiehome/Main.jsx
@@ -1,12 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import Card from "../lammiehome/Card";
-import { useState } from "react";
-import Link from "next/link";
 
 const Main = () => {
-  const [toggleMenu, setToggleMenu] = useState(false);
-  
   const products = [
     {
       id: 1,
@@ -75,12 +71,7 @@ const Main = () => {
     <>
       <main>
         <div className="flex flex-wrap justify-between items-center p-3 sm:ml-64">
-          <button 
-            className="block sm:hidden text-blue-800" 
-            onClick={() => setToggleMenu(!toggleMenu)}
-          >
-            ☰
-          </button>
+         
           <input
             type="search"
             id="default-search"
@@ -93,10 +84,10 @@ const Main = () => {
           </div>
 
           <div className="hidden sm:flex mt-10 gap-3">
-            <button className="bg-blue-800 p-1 rounded text-white">
+            <button className="bg-blue-800 p-2 rounded-xl text-white">
               Wishlist
             </button>
-            <button className="bg-blue-800 p-1 rounded text-white">
+            <button className="bg-blue-800 p-2  rounded-xl text-white">
               Your Cart
             </button>
           </div>
@@ -112,7 +103,10 @@ const Main = () => {
 
         <section className="p-3 sm:ml-72">
           <div className="flex justify-between items-center sm:hidden">
-            <div className="flex justify-center w-full">
+            <button className="block sm:hidden">
+              ☰
+            </button>
+            <div className="flex-1 flex justify-center">
               <h1 className="text-xl font-bold">Lammie</h1>
             </div>
             <div className="flex gap-3">
@@ -121,46 +115,10 @@ const Main = () => {
               <button className="text-blue-800">🛒</button>
             </div>
           </div>
-          {toggleMenu && (
-            <div className="sm:hidden flex flex-col items-center mt-4">
-              <input
-                type="search"
-                id="default-search"
-                className="block w-full sm:w-80 p-2 text-sm border rounded-lg bg-gray-50"
-                placeholder="Search Mockups, Logos..."
-                required
-              />
-              <div className="mt-4">
-                <button className="bg-blue-800 p-1 rounded text-white w-full mb-2">
-                  Wishlist
-                </button>
-                <button className="bg-blue-800 p-1 rounded text-white w-full">
-                  Your Cart
-                </button>
-              </div>
-            </div>
-          )}
-
-          <p>
-            <span>Mainpage</span> <span>Category</span> <span>Dress</span>
-          </p>
-          <div>
-            <h2 className="font-bold">Dress</h2>
-            <div className="flex gap-4">
-              <button className="bg-gray-300 p-2 rounded-xl">Midi ✖️</button>
-              <button className="bg-gray-300 p-2 rounded-xl">Black ✖️</button>
-              <button className="bg-gray-300 p-2 rounded-xl">XXL ✖️</button>
-            </div>
-          </div>
 
           <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 p-3 sm:ml-5">
             {products.map((product) => (
-              <>
-              <Link href={'/cartpage'}>
               <Card key={product.id} product={product} />
-              </Link>
-              
-              </>
             ))}
           </div>
         </section>
