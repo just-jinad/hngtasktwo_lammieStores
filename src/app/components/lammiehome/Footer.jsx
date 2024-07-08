1b68b526a1e2fa2a972c790d1470ff38eb9061cd
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-07-08 19:35:06 +0100

    updated

--- src/app/components/lammiehome/Footer.jsx
+++ src/app/components/lammiehome/Footer.jsx
@@ -1,13 +1,46 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Footer = () => {
+  const socials =[
+    {
+      id: 1,
+      image:'/twitter.png'
+    },
+    {
+      id: 2,
+      image:'/facebook.png'
+    },
+    {
+      id: 3,
+      image:'/instagram.png'
+    },
+    {
+      id: 4,
+      image:'/wifi.png'
+    },
+  ]
   return (
     <footer className='p-10 text-white' style={{ backgroundColor: "#2d16bb" }}>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <div>
           <h4 className="font-bold mb-4">CONTACT INFO</h4>
           <p>Anderson Smith street, Lekki Phase 1, Lagos, Nigeria</p>
           <p>+234 706 624 67198</p>
+         
+          <div className="grid grid-cols-4 sm:grid-cols-4 md:grid-cols-4">
+      {socials.map((data) => (
+        <div className="flex " key={data.id}>
+          <Image
+            src={data.image}
+            height={20}
+            width={20}
+            alt="socials"
+            className="bg-white "
+          />
+        </div>
+      ))}
+    </div>
         </div>
 
         <div>
